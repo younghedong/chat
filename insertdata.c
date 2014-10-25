d0ad68946da8ce084b6398fb29ce4b249c86c3ac
earlren <earlren@126.com>
2014-10-25 20:15:55 +0800

    删除创建数据库

--- insertdata.c
+++ insertdata.c
@@ -29,12 +29,12 @@ int insertUser(char *uname, char *upsd)
     }
 
     //判断数据库不存在，创建数据库和表
-    if(mysql_select_db(mysql, "chatMysql") != 0 )
+    /*if(mysql_select_db(mysql, "chatMysql") != 0 )
     {
         mysql_real_query(mysql, createdb, strlen(createdb));
         mysql_real_query(mysql, createtable, strlen(createtable));
     }
-
+    */
     //注册查询是否合法
     if(mysql_real_connect(mysql, host, user, psd, "chatMysql", port, NULL, 0))
     {
